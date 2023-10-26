69fc907eb32b2e9cd5af02d15eb996e84f988427
AmirrMahmoudi <amirhosssein.mahmoudi@gmail.com>
2023-10-26 13:39:07 +0330

    close project

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const SERVER_URL = "http://localhost:9000";
-const SERVER_URL = "http://contactsapi.mahmoudi.dev";
+const SERVER_URL = "http://localhost:9000";
+// const SERVER_URL = "http://contactsapi.mahmoudi.dev";
 
 // @desc Get All Contacts
 // @route GET http://localhost:9000/contacts
